06fb7d2ca46df81d5c87e4f5a83202159c6b4d61
Leo Herzog <xd1936@gmail.com>
2019-05-24 15:29:01 -0400

    Rework to Have Max Downloading Limit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const prettyBytes = require('pretty-bytes');
 
 var downloader = new Webtorrent();
 
-var index = 0;
+var maxDownloading = 2;
 var urls = [];
 
 if (!fs.existsSync('./downloads')) {
@@ -35,35 +35,37 @@ getJSON('https://linux.exchange/distros.json', function (error, response) {
   console.log("Starting seeding of " + urls.length + " torrents...");
 
   for (var i in urls) {
-    console.log(urls[i]);
+    // console.log(urls[i] + "\n");
     downloader.add(urls[i], { "path": "./downloads" });
   }
 
-  //startTorrent(urls[index]);
+  for (var i in downloader.torrents) {
+    downloader.torrents[i].pause();
+  }
+
+  setInterval(checkProgress, 2000);
 
 });
 
-// recursively go through each torrent and add them when done with the previous
-function startTorrent(url) {
+function checkProgress() {
 
-  //console.log("Adding " + url);
-
-  downloader.add(url, { "path": "./downloads" }, function (torrent) {
-
-    torrent.on('done', function () {
-      index++;
-      setTimeout(function () { startTorrent(urls[index]); }, 5000);
-    });
-
-  });
-
-}
-
-setInterval(function () {
-  console.log("Active Torrents: " + downloader.torrents.length + ", Active Downloads Progress: " + (downloader.progress * 100).toFixed(2) + "%, Download: " + prettyBytes(downloader.downloadSpeed) + "/s, Upload: " + prettyBytes(downloader.uploadSpeed) + "/s");
+  console.log("Total Torrents: " + downloader.torrents.length + ", Download: " + prettyBytes(downloader.downloadSpeed) + "/s, Upload: " + prettyBytes(downloader.uploadSpeed) + "/s");
   var individualprogress = "";
   for (var i in downloader.torrents) {
     individualprogress += "[" + i + ": " + (downloader.torrents[i].progress * 100).toFixed(1) + "%] ";
   }
   console.log(individualprogress);
-}, 2000);
+
+  var unfinishedTorrents = downloader.torrents.filter(function (torrent) {
+    return torrent.progress < 1;
+  });
+
+  // go through and unpause the first {maxDownloading} torrents
+  for (var i in unfinishedTorrents) {
+    if (i == maxDownloading) {
+      break;
+    }
+    unfinishedTorrents[i].resume();
+  }
+
+}
