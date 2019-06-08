362d1ccea1c2b92439d35c0301cf9f789fe0b4f5
Leo <5376265+leoherzog@users.noreply.github.com>
2019-06-07 22:09:43 -0400

    Simplify Tracker String

--- index.js
+++ index.js
@@ -23,14 +23,9 @@ getJSON('https://linux.exchange/distros.json', function (error, response) {
     process.exit();
   }
 
-  var trackerString = "";
-  for (var i in response.trackers) {
-    trackerString += "&tr=" + encodeURIComponent(response.trackers[i]);
-  }
-
   for (var i in response.distros) {
     for (var j in response.distros[i].versions) {
-      var url = response.distros[i].versions[j]["magnet-url"] + trackerString + "&ws=" + encodeURIComponent(response.distros[i].versions[j]["direct-download-url"]);
+      var url = response.distros[i].versions[j]["magnet-url"] + "&tr=" + response.trackers.join("&tr=") + "&ws=" + response.distros[i].versions[j]["direct-download-url"];
       // console.log(url + '\n');
       // fs.appendFileSync('./downloads/magnets.txt', url + '\n\n');
       urls.push(url);
